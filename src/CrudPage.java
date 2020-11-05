7fe4da27d1f5ef4b554bec661cff8464cc0caf4a
Sidharth Bajaj <w0744581@myscc.ca>
2020-11-05 00:21:30 -0500

    initial commit on main

---
+++ src/CrudPage.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CrudPage extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("java project");
+    }
+}
